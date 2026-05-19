7e600d721632c26557133909ba10a89306f62137
chuka <chuka41k@gmail.com>
2026-05-19 20:26:10 +0700

    fix: remove deprecated use_label_encoder param from XGBoost config

--- config.py
+++ config.py
@@ -25,7 +25,6 @@ XGBOOST_PARAMS = {
     "learning_rate": 0.05,
     "subsample": 0.8,
     "colsample_bytree": 0.8,
-    "use_label_encoder": False,
     "eval_metric": "logloss",
     "random_state": 42,
 }
